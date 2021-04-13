2a50a5e798ce02cb501f83c0910d46b1f5e2fba2
Ruksana Shaukat Jali <ruksanashaukat@gmail.com>
2021-04-13 15:42:00 +0100

    (WIP) Task List
    Using dummy data and mapping to prototype a rough task list screen

--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -10,34 +10,33 @@ export const getServerSideProps = async () => {
 
 const Index = ({ posts }) => {
   const [session, loading] = useSession()
-
+  console.log(posts.length)
   return (
     <Layout>
-      <h1>Posts</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Body</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
+      <h1>New Care Assessments</h1>
+      <table class="govuk-table lbh-table">
+        <tbody class="govuk-table__body">
           {posts.map(post => (
-            <tr key={post.id}>
-              <td>{post.title}</td>
-              <td>{post.content}</td>
-              <td>
-                <Link href={`/posts/${post.id}`}>
-                  <a>Show</a>
+          <div>
+           <h2 key={post.id}>{post.id}. {post.title}</h2> 
+            <tr class="govuk-table__row">
+            <td class="govuk-table__cell">{post.content}</td>
+            <td class="govuk-table__cell">{post.published}</td>
+            <td class="govuk-table__cell">
+              <span class="govuk-tag lbh-tag lbh-tag--green" >status test </span>
+            </td>
+            <td class="govuk-table__cell">
+              <Link href={`/posts/${post.id}`}>
+                <a>Show</a>
+              </Link>
+              {session && (
+                <Link href={`/posts/${post.id}/edit`}>
+                  <a>Edit</a>
                 </Link>
-                {session && (
-                  <Link href={`/posts/${post.id}/edit`}>
-                    <a>Edit</a>
-                  </Link>
-                )}
-              </td>
+              )}
+            </td>
             </tr>
+          </div>
           ))}
         </tbody>
       </table>
