a0a6f872e696283fde8c1bad6ab5726bbe3d4834
Jaye Hackett <jaye.hackett@gmail.com>
2021-04-14 10:38:33 +0100

    layout work

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,29 +1,17 @@
 import Link from "next/link"
-import { signIn, signOut, useSession } from "../lib/auth"
+import Header from "./Header"
 
 interface Props {
   children: React.ReactChild | React.ReactChildren
 }
 
 const Layout = ({ children }: Props): React.ReactElement => {
-  const [session, loading] = useSession()
-
-  if (session) {
-    return (
-      <>
-        <p>
-          You are signed in as {session.email || "Unknown email"}
-          <button onClick={() => signOut()}>Sign out</button>
-        </p>
-        {children}
-      </>
-    )
-  }
-
   return (
     <>
-      <button onClick={signIn}>Sign in</button>
-      {children}
+      <Header />
+      <main className="lbh-main-wrapper " id="main-content" role="main">
+        <div className="lbh-container">{children}</div>
+      </main>
     </>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app"
+import Head from "next/head"
 import { Provider } from "../lib/auth"
 import Layout from "../components/Layout"
 import "./index.scss"
@@ -6,6 +7,16 @@ import "./index.scss"
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>Hackney Council</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <meta name="theme-color" content="#0b0c0c" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
