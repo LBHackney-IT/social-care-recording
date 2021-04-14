1cc8520b6d70220cae8638ecf67a8d6dd7b84f30
Jaye Hackett <jaye.hackett@gmail.com>
2021-04-14 21:10:10 +0100

    fix tests

--- lib/formatters.test.ts
+++ lib/formatters.test.ts
@@ -1,13 +1,16 @@
 import { prettyDate } from "./formatters"
 
 describe("prettyDate", () => {
-  it("correctly formats an ISO date string", () => {
+  it("correctly formats ISO date strings", () => {
     const result = prettyDate("1990-04-10T00:00:00.0000000")
-    expect(result === "10 Apr 1990")
+    expect(result).toEqual("10 Apr 1990")
+
+    const result2 = prettyDate("2021-12-08T12:55:00.0000000")
+    expect(result2).toEqual("8 Dec 2021")
   })
 
   it("prints nothing if fed an unparsable string", () => {
     const result = prettyDate("blah")
-    expect(result === "")
+    expect(result).toBe("")
   })
 })
--- lib/formatters.ts
+++ lib/formatters.ts
@@ -1,9 +1,12 @@
 import { DateTime } from "luxon"
 
 export const prettyDate = (isoDateString: string): string => {
-  return DateTime.fromISO(isoDateString).toLocaleString({
-    day: "numeric",
-    month: "short",
-    year: "numeric",
-  })
+  const parsed = DateTime.fromISO(isoDateString).setLocale("en-gb")
+  return parsed.isValid
+    ? parsed.toLocaleString({
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+      })
+    : ""
 }
