94d48339117e28a667df22fa362cec8d775a7adc
Jaye Hackett <jaye.hackett@gmail.com>
2021-04-14 12:31:41 +0100

    api endpoint for a submission

--- pages/api/submissions/[id].ts
+++ pages/api/submissions/[id].ts
@@ -1,24 +1,42 @@
 import prisma from "../../../lib/prisma"
 import { getSession } from "../../../lib/auth"
 import { NextApiRequest, NextApiResponse } from "next"
+import forms from "../../../config/forms"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const session = await getSession({ req })
     if (session) {
-      //   let { formId, socialCareId } = JSON.parse(req.body)
+      let { id } = req.query
 
-      //   const newSubmission = await prisma.submission.create({
-      //     data: {
-      //       formId,
-      //       socialCareId: Number(socialCareId),
-      //       createdBy: session.email,
-      //     },
-      //   })
+      //   1. grab submission
+      const submission = await prisma.submission.findUnique({
+        where: {
+          id,
+        },
+      })
+
+      // 2. grab person
+      const res2 = await fetch(
+        `${process.env.SOCIAL_CARE_API_ENDPOINT}/residents?mosaic_id=${submission.socialCareId}`,
+        {
+          headers: {
+            "x-api-key": process.env.SOCIAL_CARE_API_KEY,
+          },
+        }
+      )
+      const data = await res2.json()
+      const person = data.residents.find(
+        resident => resident.mosaicId === submission.socialCareId
+      )
+
+      // 3. grab form
+      const form = forms.find(form => form.id === submission.formId)
+
       res.json({
-        person: false,
-        form: false,
-        answers: false,
+        person,
+        form,
+        submission,
       })
     } else {
       res.status(401).json({
