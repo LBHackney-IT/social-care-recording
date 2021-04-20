e876fa4acd9ea1a47c15736618f0efc2a01e12e5
Jaye Hackett <jaye.hackett@gmail.com>
2021-04-20 17:07:53 +0100

    add tests

--- components/CaseNoteForm.test.tsx
+++ components/CaseNoteForm.test.tsx
@@ -2,9 +2,39 @@ import CaseNoteForm from "./CaseNoteForm"
 import { render, screen, fireEvent, waitFor } from "@testing-library/react"
 
 describe("CaseNoteForm", () => {
-  it("renders correctly", () => {})
+  it("renders correctly", () => {
+    render(<CaseNoteForm onSubmit={() => {}} />)
 
-  it("shows the right conditional fields", () => {})
+    expect(screen.getByText("What kind of note is this?"))
+
+    expect(screen.queryByText("What kind of visit?")).toBeNull()
+    expect(screen.queryByText("What kind of correspondance?")).toBeNull()
+
+    expect(screen.getByLabelText("What happened?"))
+    expect(screen.getByText("Actions"))
+
+    expect(screen.getByText("Save"))
+  })
+
+  it("shows the right conditional content", () => {
+    render(<CaseNoteForm onSubmit={() => {}} />)
+
+    fireEvent.click(screen.getByLabelText("Visit"))
+    expect(screen.getByText("What kind of visit?"))
+
+    fireEvent.click(screen.getByLabelText("Correspondance"))
+    expect(screen.getByText("What kind of correspondance?"))
+
+    fireEvent.click(screen.getByLabelText("Email, letter or text message"))
+    expect(
+      screen.getByText(
+        "You don't need to copy and paste emails or text messages.",
+        {
+          exact: false,
+        }
+      )
+    )
+  })
 
   it("shows an error if submission fails", async () => {
     render(
