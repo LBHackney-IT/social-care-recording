2ca396b77ae2a1508fa023909a04f0b58edaf3a2
Jaye Hackett <jaye.hackett@gmail.com>
2021-04-20 23:02:09 +0100

    person widget supports grouped display

--- components/PersonWidget.tsx
+++ components/PersonWidget.tsx
@@ -4,9 +4,59 @@ import { prettyDate } from "../lib/formatters"
 
 interface Props {
   person: Person | false
+  grouped?: boolean
+  index?: number
+  onRemove?: (number) => void
+  open?: boolean
+  setOpen?: (number) => void
 }
 
-const PersonWidget = ({ person }: Props): React.ReactElement => {
+const PersonWidget = ({
+  person,
+  grouped,
+  onRemove,
+  index,
+  open,
+  setOpen,
+}: Props): React.ReactElement => {
+  if (grouped && person)
+    return (
+      <aside className={s.aside}>
+        <details className={s.details} open={open}>
+          <summary
+            className={s.summary}
+            onClick={e => {
+              e.preventDefault()
+              open ? setOpen(false) : setOpen(index)
+            }}
+          >
+            <h2 className={`lbh-heading-h3 ${s.title}`}>
+              {person.firstName} {person.lastName}
+            </h2>
+
+            <svg width="17" height="10" viewBox="0 0 17 10">
+              <path d="M2 1.5L8.5 7.5L15 1.5" strokeWidth="3" />
+            </svg>
+          </summary>
+          <p className={`lbh-body-s ${s.paragraph}`}>Referred 12 Feb 2021</p>
+          <p className={`lbh-body-s ${s.paragraph}`}>
+            Born {prettyDate(person.dateOfBirth)}
+          </p>
+          <p className={`lbh-body-s ${s.paragraph}`}>5 known relationships</p>
+
+          <p className={`lbh-body-s ${s.important}`}>
+            <strong>1 warning</strong> <span>2 open actions</span>
+          </p>
+          <button
+            className="lbh-link lbh-body-s"
+            onClick={() => onRemove(index)}
+          >
+            Remove
+          </button>
+        </details>
+      </aside>
+    )
+
   if (person)
     return (
       <aside className={s.aside}>
