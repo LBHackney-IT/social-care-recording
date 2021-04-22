c6e7c9cad9c6b420d857cc91222f746f21cb2c33
Jaye Hackett <jaye.hackett@gmail.com>
2021-04-22 21:25:02 +0100

    fix tests

--- components/GroupRecordingWidget.test.tsx
+++ components/GroupRecordingWidget.test.tsx
@@ -3,8 +3,24 @@ import { fireEvent, render, screen } from "@testing-library/react"
 
 // TODO: once the group recording widget is production ready, write its tests
 
-// describe("GroupRecordingWidget", () => {
-//   it("shows one person intitially", () => {})
-//   it("allows people to be added and removed", () => {})
-//   it("allows widgets to be expanded and collapsed when there are more than one", () => {})
-// })
+const mockPerson = {
+  mosaicId: "1",
+  firstName: "Bart",
+  lastName: "Simpson",
+  dateOfBirth: "1990-04-10T00:00:00.0000000",
+  addressList: [
+    {
+      addressLine1: "123 Town St, Citythorpe, AB1 23C",
+    },
+  ],
+}
+
+describe("GroupRecordingWidget", () => {
+  it("shows one person intitially", () => {
+    render(<GroupRecordingWidget initialPerson={mockPerson} />)
+    expect(screen.getByText("Bart Simpson"))
+    expect(screen.getByText("Add another person"))
+  })
+  //   it("allows people to be added and removed", () => {})
+  //   it("allows widgets to be expanded and collapsed when there are more than one", () => {})
+})
