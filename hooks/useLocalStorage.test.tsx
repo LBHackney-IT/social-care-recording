44dfd7643aecd3111f0fdc73509de02e529d91c4
Jaye Hackett <jaye.hackett@gmail.com>
2021-04-22 08:56:49 +0100

    add tests for useLocalStorage hook

--- hooks/useLocalStorage.test.tsx
+++ hooks/useLocalStorage.test.tsx
@@ -1,13 +1,46 @@
 import useLocalStorage from "./useLocalStorage"
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
+
+beforeAll(() => {
+  global.Storage.prototype.setItem = jest.fn()
+
+  global.Storage.prototype.getItem = jest
+    .fn()
+    .mockReturnValueOnce(undefined)
+    .mockReturnValueOnce(JSON.stringify("saved value"))
+    .mockReturnValueOnce(undefined)
+})
 
 const MockComponent = () => {
-  const [foo, setFoo] = useLocalStorage("foo", false)
-  return null
+  const [foo, setFoo] = useLocalStorage("foo", "initial value")
+  return (
+    <>
+      {foo}
+      <button onClick={() => setFoo("new value")} />
+    </>
+  )
 }
 
 describe("useLocalStorage", () => {
-  it("", () => {
+  it("returns the default value if there is nothing in localstorage", () => {
     render(<MockComponent />)
+    expect(screen.getByText("initial value"))
+    expect(global.Storage.prototype.getItem).toBeCalled()
+  })
+
+  it("it returns the saved value if in localstorage", () => {
+    render(<MockComponent />)
+    expect(screen.getByText("saved value"))
+    expect(global.Storage.prototype.getItem).toBeCalled()
+  })
+
+  it("displays and saves a changed value", () => {
+    render(<MockComponent />)
+    fireEvent.click(screen.getByRole("button"))
+    expect(screen.getByText("new value"))
+    expect(global.Storage.prototype.setItem).toBeCalledWith(
+      "foo",
+      JSON.stringify("new value")
+    )
   })
 })
