a49b5f80c74fbb2105a5bae02ee0c65cf8f577aa
Jaye Hackett <jaye.hackett@gmail.com>
2021-05-03 11:13:57 +0100

    clean up homepage layout, refactor submissions table

---
+++ pages/new.tsx
@@ -0,0 +1,78 @@
+import { GetServerSideProps } from "next"
+import StartForm from "../components/StartForm"
+import { useRouter } from "next/router"
+import Link from "next/link"
+import { Submission } from "@prisma/client"
+import { Form } from "../config/forms.types"
+import SubmissionsTable, {
+  SubmissionWithForm,
+} from "../components/SubmissionsTable"
+import { getSession } from "../lib/auth"
+
+interface Props {
+  forms: Form[]
+}
+
+const StartPage = ({ forms }: Props) => {
+  const router = useRouter()
+
+  const handleSubmit = async (values, { setStatus }) => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/submissions`,
+        {
+          method: "POST",
+          body: JSON.stringify(values),
+        }
+      )
+      const data = await res.json()
+      if (data.error) throw data.error
+      router.push(`/submissions/${data.id}`)
+    } catch (e) {
+      setStatus(e.toString())
+    }
+  }
+
+  return (
+    <div>
+      <h1 className="govuk-heading-h1 govuk-!-margin-bottom-8">
+        Record something new
+      </h1>
+
+      {forms && <StartForm onSubmit={handleSubmit} forms={forms} />}
+    </div>
+  )
+}
+
+StartPage.Postheader = ({ params }): React.ReactElement => (
+  <div className="lbh-container">
+    <Link href={`/`}>
+      <a className="govuk-back-link lbh-back-link">Go back</a>
+    </Link>
+  </div>
+)
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  if (!getSession({ req })) {
+    return {
+      props: {},
+      redirect: {
+        destination: "/sign-in",
+      },
+    }
+  }
+
+  const res2 = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api`, {
+    headers: {
+      cookie: req.headers.cookie,
+    },
+  })
+  const data = await res2.json()
+  return {
+    props: {
+      ...data,
+    },
+  }
+}
+
+export default StartPage
