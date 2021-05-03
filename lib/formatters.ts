94c5bb9707b053c48b0986719a03c7dcb68f6fbd
Jaye Hackett <jaye.hackett@gmail.com>
2021-05-03 11:12:31 +0100

    add prettyDateAndTime method

--- lib/formatters.ts
+++ lib/formatters.ts
@@ -6,6 +6,12 @@ export const prettyDate = (isoDateString: string): string => {
   return parsed.isValid ? parsed.toFormat("d MMM yyyy") : ""
 }
 
+/** Convert an ISO-formatted string into a human-friendly date string */
+export const prettyDateAndTime = (isoDateString: string): string => {
+  const parsed = DateTime.fromISO(isoDateString)
+  return parsed.isValid ? parsed.toFormat("d MMM yyyy h.mm a") : ""
+}
+
 /** Convert a millisecond string into a human-friendly date string */
 export const prettyDateFromMillis = (millis: number): string => {
   const parsed = DateTime.fromMillis(millis)
