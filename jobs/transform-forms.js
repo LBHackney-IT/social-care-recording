8fb369362c575d2233fc76110259cef08a7be782
Jaye <joshua.hackett93@gmail.com>
2021-05-04 15:24:52 +0100

    wip

--- jobs/transform-forms.js
+++ jobs/transform-forms.js
@@ -4,75 +4,28 @@ const fs = require("fs")
 
 const slugify = string => rawSlugify(string, { lower: true })
 
-const mapIfUnique = (rawData, processedKey, rawKey, filterFunc, cb) =>
-  rawData.filter(filterFunc).reduce((accumulator, formRow) => {
-    if (accumulator.find(el => el[processedKey] === formRow[rawKey])) {
-      return accumulator
-    } else {
-      return cb(accumulator, formRow)
-    }
-  }, [])
-
 // take a flat csv and turn it into the json format we need for our forms config
 const run = async () => {
   try {
-    const rawData = await csv().fromFile("./jobs/data.csv")
+    const rows = await csv().fromFile("./jobs/data.csv")
     // remove example row from data
-    rawData.shift()
+    rows.shift()
 
-    const forms = mapIfUnique(
-      rawData,
-      "name",
-      "Form name",
-      el => true,
-      (accumulator, formRow) =>
-        accumulator.concat({
-          id: slugify(formRow["Form name"]),
-          name: formRow["Form name"],
-
-          steps: mapIfUnique(
-            rawData,
-            "name",
-            "Topic of questions (Step)",
-            stepRow => stepRow["Form name"] === formRow["Form name"],
-            (accumulator, stepRow) =>
-              accumulator.concat({
-                id: slugify(stepRow["Topic of questions (Step)"]),
-                name: stepRow["Topic of questions (Step)"],
-                theme: stepRow["Header (Theme)"],
-
-                fields: mapIfUnique(
-                  rawData,
-                  "question",
-                  "Question",
-                  qRow =>
-                    qRow["Topic of questions (Step)"] ===
-                    stepRow["Topic of questions (Step)"],
-                  (accumulator, qRow) =>
-                    accumulator.concat({
-                      id: slugify(qRow["Question"]),
-                      question: qRow["Question"],
-                      type: qRow["Input type"],
-                      required: qRow["Required?"] === "true",
-                      hint: qRow["Hints"],
-                      error: qRow["Custom error message"],
-                      choices: qRow["Options"]
-                        .split(",")
-                        .filter(el => el)
-                        .map(choice => ({
-                          value: slugify(choice),
-                          label: choice,
-                        })),
-                      // TODO: handle these
-                      // prefill
-                      // className
-                      // condition
-                    })
-                ),
-              })
-          ),
-        })
-    )
+    const forms = rows.map(row => ({
+      id: slugify(qRow["Question"]),
+      question: qRow["Question"],
+      type: qRow["Input type"],
+      required: qRow["Required?"] === "true",
+      hint: qRow["Hints"],
+      error: qRow["Custom error message"],
+      choices: qRow["Options"]
+        .split(",")
+        .filter(el => el)
+        .map(choice => ({
+          value: slugify(choice),
+          label: choice,
+        })),
+    }))
 
     fs.writeFileSync("./jobs/data.json", JSON.stringify(forms))
   } catch (e) {
