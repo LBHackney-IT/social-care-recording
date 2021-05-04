65fdd939dbd8c0c7963be24ff3a90edf9bc51d50
Jaye Hackett <jaye.hackett@gmail.com>
2021-05-04 19:05:59 +0100

    refactor form job

--- jobs/transform-forms.js
+++ jobs/transform-forms.js
@@ -8,10 +8,10 @@ const slugify = string => rawSlugify(string, { lower: true })
 const run = async () => {
   try {
     const rows = await csv().fromFile("./jobs/data.csv")
-    // remove example row from data
+    // 1. remove example row from data
     rows.shift()
 
-    // 1. get an array of every field
+    // 2. get an array of every field
     const allFields = rows.map(row => ({
       id: slugify(row["Question"]),
       question: row["Question"],
@@ -20,7 +20,7 @@ const run = async () => {
       hint: row["Hints"],
       error: row["Custom error message"],
       prefill: row["Prefill from"],
-      choices: qRow["Options"]
+      choices: row["Options"]
         .split(",")
         .filter(el => el)
         .map(choice => ({
@@ -31,9 +31,39 @@ const run = async () => {
         id: row["Shows up based on this question"],
         value: row["Shows up if question in column H has has value"],
       },
+      // use these in the next step
+      step: row["Topic of questions (Step)"],
+      theme: row["Header (Theme)"],
+      stepDescription:
+        row["More detail about topic of questions (Step description)"],
     }))
 
-    fs.writeFileSync("./jobs/data.json", JSON.stringify(forms))
+    // 3. group the fields by step
+    const allSteps = allFields.reduce((steps, field) => {
+      const { step, theme, stepDescription } = field
+      const existingStep = steps.find(step2 => step2.name === step)
+      if (existingStep) {
+        existingStep.fields.push(field)
+      } else {
+        steps.push({
+          id: slugify(step),
+          name: step,
+          theme: theme,
+          stepDescription: stepDescription,
+          fields: [field],
+        })
+      }
+      return steps
+    }, [])
+
+    // 4. wrap steps in the form
+    const form = {
+      id: slugify(rows[0]["Form name"]),
+      name: rows[0]["Form name"],
+      steps: allSteps,
+    }
+
+    fs.writeFileSync("./jobs/data.json", JSON.stringify(form))
   } catch (e) {
     console.error(e)
   }
