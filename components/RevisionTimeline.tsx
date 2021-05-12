3803b9cf497ff8d9812b610493cf7e7af1b2b8ed
Jaye Hackett <jaye.hackett@gmail.com>
2021-05-12 09:28:51 +0100

    bail out if no revisions to show

--- components/RevisionTimeline.tsx
+++ components/RevisionTimeline.tsx
@@ -10,22 +10,26 @@ interface Props {
 const RevisionTimeline = ({
   revisions,
   totalSteps,
-}: Props): React.ReactElement => (
-  <ol className={`lbh-timeline ${s.timeline}`}>
-    {revisions.map(revision => (
-      <li
-        className={`lbh-timeline__event lbh-timeline__event--minor ${s.event}`}
-        key={revision.id}
-      >
-        <h3 className="lbh-body">{revision.createdBy}</h3>
+}: Props): React.ReactElement => {
+  if (!revisions || revisions.length === 0) return null
 
-        <p className="lbh-body-xs">
-          {prettyDateAndTime(revision.createdAt.toString())} (
-          {Math.round((revision.completedSteps.length / totalSteps) * 100)}%)
-        </p>
-      </li>
-    ))}
-  </ol>
-)
+  return (
+    <ol className={`lbh-timeline ${s.timeline}`}>
+      {revisions.map(revision => (
+        <li
+          className={`lbh-timeline__event lbh-timeline__event--minor ${s.event}`}
+          key={revision.id}
+        >
+          <h3 className="lbh-body">{revision.createdBy}</h3>
+
+          <p className="lbh-body-xs">
+            {prettyDateAndTime(revision.createdAt.toString())} (
+            {Math.round((revision.completedSteps.length / totalSteps) * 100)}%)
+          </p>
+        </li>
+      ))}
+    </ol>
+  )
+}
 
 export default RevisionTimeline
